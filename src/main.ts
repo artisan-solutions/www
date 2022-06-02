a3291763f22e3c65ff09288ef157bb08ebb6e9aa
piet <tphollebeek@gmail.com>
2022-06-03 01:52:35 +0200

    style changes

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./style.scss";
+
+const resizeScrollPadding = () => {
+    const navigationHeight =
+        document.querySelector<HTMLElement>("#primary-navigation")?.offsetTop ??
+        "4rem";
+
+    document.documentElement.style.setProperty(
+        "--scroll-padding",
+        navigationHeight.toString()
+    );
+};
+
+window.addEventListener("resize", resizeScrollPadding);
+resizeScrollPadding();
